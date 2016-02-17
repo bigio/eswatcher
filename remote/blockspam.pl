d651792c4581983b2e4aff082cce33a8f8f60d3a
Giovanni Bechis <giovanni@paclan.it>
2016-02-17 12:38:03 +0100

    Do not run the script as root concatenate an XXX with the real password to disable the account use sudo(8) to execute pfctl(8)

--- remote/blockspam.pl
+++ remote/blockspam.pl
@@ -21,13 +21,15 @@ if ($#ARGV != 1) {
 my $ip=shift;
 my $email=shift;
 
+die "Script should not run as root, use sudo instead\n" if ( $< == 0 );
+
 if ( $^O eq "openbsd" ) {
-	system("/sbin/pfctl -k $ip");
+	system("/usr/local/bin/sudo /sbin/pfctl -k $ip");
 }
 
 $conn = DBI->connect("DBI:mysql:database=$db;host=$host",$user,$pass,{RaiseError => 1});
 
-$query = $conn->prepare("UPDATE mail_user SET password='XXX' WHERE email = $email");
+$query = $conn->prepare("UPDATE mail_user SET password=CONCAT('XXX ', password) WHERE email = \"$email\"");
 $query->execute();
 $query->finish();
 $conn->disconnect();
